668bb0e6005369e1ea4bab000e7b9fe9269eb2aa
opilane <opilane@HP-CZC2349HTC>
2026-01-13 15:38:31 +0200

    new

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,9 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using project.Models;
+using System.IO;
+
 using Microsoft.Owin.Security.Provider;
+using System.Web.Helpers;
+using System.Web.UI.WebControls;
 namespace project.Controllers
 {
     public class HomeController : Controller
@@ -52,6 +57,7 @@ namespace project.Controllers
             ViewBag.Pyhad = new SelectList(pyhad, "Id", "Nimetus", kylaline.PyhaId);
             return View(kylaline);
         }
+
         public ActionResult Tanan(int id)
         {
             var kylaline = db.Kylalined.Find(id);
@@ -60,12 +66,60 @@ namespace project.Controllers
                 return HttpNotFound();
             }
             ViewBag.Pyhanimetus = db.Pyhad.Find(kylaline.PyhaId)?.Nimetus;
-            ViewBag.Pilt = "smile.jpg";
+            if (kylaline.OnKutse)
+            {
+                ViewBag.Pilt = "smile.jpg";
+            }
+            else
+            { ViewBag.Pilt = "sad.jpg"; }
+            SaadaEmail(kylaline, ViewBag.Pilt, ViewBag.Pyhanimetus, kylaline.OnKutse);
 
-
-            ViewBag.Pyhanimetus = db.Pyhad.Find(kylaline.PyhaId)?.Nimetus;
-            ViewBag.Pilt2 = "sad.jpg";
             return View("Tanan", kylaline);
         }
+
+        private void SaadaEmail(Kylaline kylaline, string pilt, string pyha, bool onkutse)
+        {
+            string failiTee = Path.Combine(Server.MapPath("~/images/"), pilt);
+            try
+            {
+                WebMail.SmtpServer = "smtp.gmail.com";
+                WebMail.SmtpPort = 587;
+                WebMail.EnableSsl = true;
+                WebMail.UserName = "eha20082@gmail.com";
+                WebMail.Password = "bwdn mlju pndo jney";
+                WebMail.From = "eha20082@gmail.com";
+
+                string sisu = "";
+                if (onkutse)
+                {
+                    sisu = $"Tere, {kylaline.Nimi}!<br/><br/>" +
+                           $"Sinu registreerumine sündmusele <b>{pyha}</b> on salvestatud.<br/>" +
+                           "Lisaksime kirjale ka sündmuse kutse. Ootame sind väga!<br/><br/>" +
+                           "Kohtumiseni!";
+                }
+                else
+                {
+                    sisu = $"Tere, {kylaline.Nimi}!<br/><br/>" +
+                           $"Sinu registreerumine sündmusele <b>{pyha}</b> on salvestatud.<br/>" +
+                           "Lisaksime kirjale ka sündmuse kutse. Kahju, et sa ei tule peole!<br/><br/>" +
+                           "Kõige head!";
+                }
+
+                // Saada kiri koos manusega
+                WebMail.Send(
+                    to: kylaline.Email,
+                    subject: "Vastus: " + pyha,
+                    body: sisu,
+                    isBodyHtml: true,
+                    filesToAttach: new string[] { failiTee } 
+                );
+            }
+            catch (System.Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("viga");
+            }
+
+        }
+
     }
 }
