35e9b380ee6dbb67594834f026333003f8c1cea2
opilane <opilane@HP-CZC2349HTC>
2026-01-15 11:33:40 +0200

    туц

---
+++ Controllers/KylalinesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using project.Models;
+
+namespace project.Controllers
+{
+    public class KylalinesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Kylalines
+        public ActionResult Index()
+        {
+            return View(db.Kylalined.ToList());
+        }
+
+        // GET: Kylalines/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kylaline kylaline = db.Kylalined.Find(id);
+            if (kylaline == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kylaline);
+        }
+
+        // GET: Kylalines/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Kylalines/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nimi,Email,OnKutse,PyhaId")] Kylaline kylaline)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Kylalined.Add(kylaline);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(kylaline);
+        }
+
+        // GET: Kylalines/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kylaline kylaline = db.Kylalined.Find(id);
+            if (kylaline == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kylaline);
+        }
+
+        // POST: Kylalines/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nimi,Email,OnKutse,PyhaId")] Kylaline kylaline)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kylaline).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kylaline);
+        }
+
+        // GET: Kylalines/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kylaline kylaline = db.Kylalined.Find(id);
+            if (kylaline == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kylaline);
+        }
+
+        // POST: Kylalines/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Kylaline kylaline = db.Kylalined.Find(id);
+            db.Kylalined.Remove(kylaline);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,50 @@
+﻿namespace project.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using project.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<project.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(project.Models.ApplicationDbContext context)
+        {
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+            // 1. Loo "Admin" roll, kui seda veel pole
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                var role = new IdentityRole { Name = "Admin" };
+                roleManager.Create(role);
+            }
+
+            // 2. Loo administraatori kasutaja
+            var adminEmail = "eha20082@gmail.com";
+            var adminUser = context.Users.FirstOrDefault(u => u.Email == adminEmail);
+
+            if (adminUser == null)
+            {
+                var user = new ApplicationUser { UserName = adminEmail, Email = adminEmail };
+                userManager.Create(user, "Parool123!"); // Vali turvaline parool
+                adminUser = user;
+            }
+
+            // 3. Seo kasutaja Admin rolliga
+            if (!userManager.IsInRole(adminUser.Id, "Admin"))
+            {
+                userManager.AddToRole(adminUser.Id, "Admin");
+            }
+        }
+    }
+}
